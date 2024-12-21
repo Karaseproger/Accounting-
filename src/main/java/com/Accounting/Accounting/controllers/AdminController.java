efbc0cf0e97593bfa093a49fd42f3cbd3bf83387
Administrator <kotvietnam@mail.ru>
2024-12-21 17:34:57 +0500

    стили под политех

--- src/main/java/com/Accounting/Accounting/controllers/AdminController.java
+++ src/main/java/com/Accounting/Accounting/controllers/AdminController.java
@@ -6,6 +6,7 @@ import com.Accounting.Accounting.repozitory.UseresRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
